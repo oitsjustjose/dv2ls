a1b7917d9b75ae785ee0cf85c88caadf0ed401f2
oitsjustjose <stovallj1995@gmail.com>
2023-03-28 23:40:25 -0500

    --

--- frontend/src/components/TextRender.jsx
+++ frontend/src/components/TextRender.jsx
@@ -13,7 +13,7 @@ export default ({ paste, syntax }) => (
         theme="github"
         name="paste_viewer"
         fontSize={14}
-        value={paste || 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eum labore optio quidem rerum consectetur perspiciatis, voluptates, aspernatur repellendus modi culpa, nam dolor cupiditate consequuntur? Dolorum sit deserunt assumenda commodi cum nihil iure sint repudiandae quos quod a architecto dicta eius excepturi nulla natus, quam voluptatum nemo itaque laudantium magnam. Sequi, beatae a debitis iusto velit porro blanditiis! Veritatis id nesciunt fugiat officia maxime rerum, exercitationem ratione doloremque atque, explicabo consequuntur! Minus, at saepe magni qui nobis earum, tempore optio in, sequi aliquid facilis accusamus ea amet minima. Aut eveniet similique ducimus ea illum dolorem expedita sapiente, ex culpa unde.'}
+        value={paste}
         wrapEnabled
         readOnly
         setOptions={{
